0bbb3a0e1e1404c51c7a8b2afddd969da430149f
Joanne Tang <joannetangrd@gmail.com>
2022-05-01 21:33:52 -0400

    style update

--- Labs/Space_Invaders/app.js
+++ Labs/Space_Invaders/app.js
@@ -35,7 +35,7 @@ console.log(ussAssembly);
 let alienMob = [];
 createAlienMob();
 
-console.log(alienMob);
+// console.log(alienMob);
 
 setAlienHealth();
 
@@ -48,7 +48,7 @@ const battle = () => {
 
   let alienScore = 100;
 
-  console.log("Battle Started!");
+  // console.log("Battle Started!");
   // startGame
 
   let command = prompt("Ready to [A]ttack? or Retreat?");
@@ -59,23 +59,24 @@ const battle = () => {
     explosion.style.width = "40px";
     explosion.style.marginLeft = "37px";
 
-    // signal explosion
-
     //decrease alien Point
 
     let shooter = document.querySelector(".fireball");
     shooter.classList.toggle("initiateAmmo");
 
     let shipHealth = document.getElementById("shipHP");
+
     // ussAssembly shooting alien ship
     let alienHealth = document.getElementById("progressBar" + targetCounter);
     if (Math.random() < ussAssembly.accuracy) {
-      console.log("ship with accurate hit");
+      // console.log("ship with accurate hit");
       alienMob[targetCounter - 1].hp -= ussAssembly.firepower;
       setTimeout(() => {
         updateAlienHealth();
       }, 2000);
       console.log("alien mob hp", alienMob[targetCounter - 1].hp);
+    } else {
+      console.log("USS Assembly missed");
     }
 
     if (alienMob[targetCounter - 1].hp > 0) {
@@ -84,12 +85,14 @@ const battle = () => {
       alienShooter.classList.toggle("initiateAlienAttack");
       // probability of alien hitting
       if (Math.random() < alienMob[targetCounter - 1].accuracy) {
-        console.log("alien with accurate hit");
+        // console.log("alien with accurate hit");
         ussAssembly.hp -= alienMob[targetCounter - 1].firepower;
         console.log("ship hp", ussAssembly);
         setTimeout(() => {
           shipHealth.value = ussAssembly.hp;
         }, 3000);
+      } else {
+        console.log("Alien missed");
       }
     } else if (alienMob[targetCounter - 1].hp <= 0) {
       //explode ufo
@@ -100,31 +103,15 @@ const battle = () => {
       shooter.classList.toggle("initiateAmmo");
     }, 3500);
     console.log("target counter", targetCounter);
-    setTimeout(() => checkIfWin(), 3500);
-    setTimeout(() => checkIfLose(), 3500);
+    setTimeout(() => checkIfWin(), 4500);
+    setTimeout(() => checkIfLose(), 4500);
   } else {
     console.log("PlayerOne has retreated and loses!");
     window.alert("PlayerOne has retreated and loses!");
   }
 };
 
-//
-function checkIfWin() {
-  if (targetCounter >= 7) {
-    const winner = document.querySelector(".winner");
-    winner.style.visibility = "visible";
-    console.log("You WIN!!!");
-  }
-}
-
-function checkIfLose() {
-  if (ussAssembly.hp === 0) {
-    const loser = document.querySelector(".loser");
-    loser.style.visibility = "visible";
-    console.log("You lose");
-  }
-}
-
+//-------------------------------------------------------
 function createAlienMob() {
   for (let i = 1; i < 7; i++) {
     let alienShip = new AlienShip(
@@ -150,6 +137,22 @@ function updateAlienHealth() {
     let progressBar = document.getElementById("progressBar" + [i + 1]);
     progressBar.value = alienMob[i].hp;
     // console.log(progressBar);
+  }
+}
+
+function checkIfWin() {
+  if (targetCounter >= 7) {
+    const winner = document.querySelector(".winner");
+    winner.style.visibility = "visible";
+    console.log("You WIN!!!");
+  }
+}
+
+function checkIfLose() {
+  if (ussAssembly.hp === 0) {
+    const loser = document.querySelector(".loser");
+    loser.style.visibility = "visible";
+    console.log("You lose");
   }
 }
 
