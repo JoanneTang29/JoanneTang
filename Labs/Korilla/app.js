07218e55e64ab36983498d43f87156058b862633
Joanne Tang <joannetangrd@gmail.com>
2022-06-09 13:36:49 -0400

    korilla lab

---
+++ Labs/Korilla/app.js
@@ -0,0 +1,69 @@
+class Receipt extends React.Component {
+  state = {
+    paid: false,
+  };
+
+  handlePaidToggle = () => {
+    this.setState({
+      paid: !this.state.paid,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>{this.props.receipt.person}</h2>
+        <h5>
+          <span>Main: </span>
+          {this.props.receipt.order.main}
+        </h5>
+        <h5>
+          <span>Protein: </span>
+          {this.props.receipt.order.protein}
+        </h5>
+        <h5>
+          <span>Rice: </span>
+          {this.props.receipt.order.sauce}
+        </h5>
+        <h5>
+          <span>Drink: </span>
+          {this.props.receipt.order.drink}
+        </h5>
+        <h5>
+          <span>Cost: </span>
+          {this.props.receipt.order.cost}
+        </h5>
+        <button
+          onClick={() => {
+            return this.props.handlePaidToggle(this.props.element);
+          }}
+        >
+          Paid
+        </button>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      receipts: receipts,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1 className="truck-name">Korilla</h1>
+        <div className="receipt-container">
+          {this.state.receipts.map((receipt) =>
+            receipt.paid ? '' : <Receipt receipt={receipt} />
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
